67ba548df8d323a84b492f765698e01d661126c9
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-05-16 12:52:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const text = document.getElementById("text");
-const delay = document.getElementById("delay");
-const btn = document.getElementById("btn");
-const output = document.getElementById("output");
+const inputText = document.getElementById('text');
+const inputDelay = document.getElementById('delay');
+const button = document.getElementById('btn');
+const outputDiv = document.getElementById('output');
 
-//your code here
-async function showMessage() {
-  const message = text.value;
-  const delayVal = delay.value;
-  await new Promise((resolve) => setTimeout(resolve, delayVal));
-  output.innerText = message;
+// Define an async function that waits for a specified time before resolving with a message
+async function displayMessageAfterDelay(message, delay) {
+  await new Promise((resolve) => setTimeout(resolve, delay));
+  return message;
 }
 
-btn.addEventListener("click", showMessage);
+// Add an event listener to the button
+button.addEventListener('click', async () => {
+  // Get the values from the input fields
+  const message = inputText.value;
+  const delay = inputDelay.value;
+
+  // Call the async function to display the message after the delay
+  const result = await displayMessageAfterDelay(message, delay);
+
+  // Display the result in the output div
+  outputDiv.innerText = result;
+});
